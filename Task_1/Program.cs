2d02d919a72dfc6b3dbc82b45fa1ca8c8bcd2cb9
«Kamila» <milo4-ka01@yandex.ru>
2022-04-02 19:10:43 +0300

    Задание 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+
+void FindPoint(int a1, int c1, int a2, int c2)
+{
+    if (Math.Abs(a1) == Math.Abs(a2))
+    {
+        if (c1 == c2) Console.WriteLine("Прямые совпадают");
+        else Console.WriteLine("Прямые параллельны ");
+    }
+    else Console.WriteLine($"Координаты:{((c2 - c1) / (a1 - a2))},{a1 * (c2 - c1) / (a1 - a2) + c1}");
+}
+Console.WriteLine("Введите k1,b1,k2,b2: ");
+int k1 = int.Parse(Console.ReadLine()?? "0");
+int b1 = int.Parse(Console.ReadLine()?? "0");
+int k2 = int.Parse(Console.ReadLine()?? "0");
+int b2 = int.Parse(Console.ReadLine()?? "0");
+FindPoint(k1, b1, k2, b2);
