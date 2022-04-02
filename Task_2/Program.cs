36351210cadff451a0801e37ac89237bbd6c1e19
«Kamila» <milo4-ka01@yandex.ru>
2022-04-02 19:31:53 +0300

    Задание 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать числа Фибоначчи
+int n = 100;
+int a1 = 0;
+int a2 = 1;
+int temp = 0;
+Console.Write($"{a1} {a2} ");
+while (a1 + a2 <= n)
+{
+    Console.Write((a1+a2) + " ");
+    temp = a2;
+    a2 = a1 + a2;
+    a1 = temp;
+}
